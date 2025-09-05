b557de3fc1cf019899993c0ae39657d04156d8c9
unknown <ous.abouras@gmail.com>
2025-09-05 13:04:30 -0700

    css fixes

--- my-app/next.config.ts
+++ my-app/next.config.ts
@@ -2,7 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  assetPrefix: 'https://<Account-name>/<Repository-name>',
   output: "export",
+  reactStrictMode: true,
 };
 
 export default nextConfig;

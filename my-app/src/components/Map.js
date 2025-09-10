e876a53927c94f8c96dbc9393102800221f38646
Selin Dursun <selindursunus@gmail.com>
2025-09-10 11:26:53 -0700

    -

--- my-app/src/components/Map.js
+++ my-app/src/components/Map.js
@@ -105,9 +105,9 @@ export default function Map() {
 
         map.current = new maplibregl.Map({
             container: mapContainer.current,
-            style: `https://api.maptiler.com/maps/dataviz-dark/style.json?key=${API_KEY}`,
+            // style: `https://api.maptiler.com/maps/dataviz-dark/style.json?key=${API_KEY}`,
             // style: `https://api.maptiler.com/maps/019934cc-16f4-70e2-            b59a-96734dcc38bf/?key=${API_KEY2}`,
-            // style: `https://api.maptiler.com/maps/019934cc-16f4-70e2-b59a-96734dcc38bf/?key=${API_KEY2}`,
+            style: `https://api.maptiler.com/maps/019934cc-16f4-70e2-b59a-96734dcc38bf/style.json?key=pWuuKuOsL6jBB1Gt1ClK`,
             zoom: initialState.zoom,
         });
 
